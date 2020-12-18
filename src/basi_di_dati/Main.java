5e53c70f87fa1a71750cf1f205cb9cb1ab78e091
Giovanni Prisco <gprisco01@gmail.com>
2020-12-18 13:19:57 +0100

    Started building interactive cli

--- src/basi_di_dati/Main.java
+++ src/basi_di_dati/Main.java
@@ -1,12 +1,56 @@
 package basi_di_dati;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			TestSuite.start();
-		} catch (Exception e) {
-			e.printStackTrace();
+		InputStreamReader keyIS;
+		BufferedReader keyBR;
+		Integer i = 0;
+		String choice = null;
+
+		keyIS = new InputStreamReader(System.in);
+		keyBR = new BufferedReader(keyIS);
+
+		while (i != 1000) {
+			System.out.println("\nScegli un'opzione:");
+			System.out.println("\t1 - Avvia test");
+			System.out.println("\t2 - Opzione");
+			System.out.println("\t1000 - Esci");
+
+			System.out.print("\nScegli: ");
+
+			try {
+				choice = keyBR.readLine();
+			} catch (IOException e1) {
+				i = 999;
+			}
+
+			try {
+				i = Integer.parseInt(choice);
+			} catch (NumberFormatException e) {
+				i = 999;
+			}
+
+			switch (i) {
+			case 1: {
+				TestSuite.start();
+				break;
+			}
+			case 2: {
+				break;
+			}
+			case 1000: {
+				break;
+			}
+			default: {
+				System.out.println("Scelta non presente");
+				break;
+			}
+			}
 		}
 	}
 }
